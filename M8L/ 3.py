c86733be061f88fb1e62783307d7e82181a94856
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-18 16:07:54 -0300

    Update  3.py

--- M8L/ 3.py
+++ M8L/ 3.py
@@ -1,24 +1,51 @@
-""'''3. Escreva um programa com uma função que, dado um número inteiro (n > 1), retorne
-uma lista com os fatores primos de n.
-'''
+'''Faça um programa que calcule o imposto de renda de um contribuinte de um país imaginário
+onde as regras do imposto são as seguintes:
+• Todos pagam a mesma alíquota, de 20%.
+• São descontados da base de cálculo (proventos) as despesas com educação e despesas
+médicas.
+• São descontados R$ 1000,00 por dependente.
+• O imposto devido ou a receber pode ser parcelado em até 6 vezes.
+• Valores de imposto (devido ou a receber) abaixo de R$100,00 não são cobrados nem
+pagos.'''
 
-def fatores_primos(n):
-    fatores = []
-    divisor = 2
-    while n > 1:
-        while n % divisor == 0:
-            fatores.append(divisor)
-            n //= divisor
-        divisor += 1
-    return fatores
+ALIQUOTA = 0.2
+DESCONTO_POR_DEPENDENTE = 1000
 
-# numero de entrada do usuario
+def calcImposto(salario, despesas, n_dependentes):
+
+    imposto_bruto = (salario * 12)*ALIQUOTA
+
+    imposto_liquido = imposto_bruto - despesas - n_dependentes*DESCONTO_POR_DEPENDENTE
+
+    return imposto_liquido
+
+def verifSituacaoPagamento(imposto):
+    if not -100 < imposto < 100:
+        if imposto > 0:
+            print(f"Você deve pagar R${round(imposto, 2):.2f}, podendo parcelar em até 6x de R${round(imposto/6, 2):.2f}")
+            print('-'*60)
+        else:
+            print(f"Você deve receber R${round(imposto*(-1), 2):.2f}, podendo receber em até 6x de R${round((imposto*-1)/6, 2):.2f}")
+            print('-'*60)
+    else:
+        print("Você não tem valores a receber ou pagar")
+        print('-'*60)
 
 def main():
-   while True:
+
+    while True:
         try:
-            numero=int(input('insira um numero para descobrir os fatores primos dele: '))
-            print(fatores_primos(numero))  # Saída: 
+            print('-'*60)
+            salario = float(input("Insira seu salário: "))
+            despesas = float(input("Insira a soma de suas despesas médicas e relacionadas a educação: "))
+            n_dependentes = int(input("Insira quantos dependentes você possui: "))
+            print('\n')
+            break
         except ValueError:
-            print("insira uma numero válido")
+            print("Valor inválido")
+
+    imposto = calcImposto(salario, despesas, n_dependentes)
+
+    verifSituacaoPagamento(imposto)
+
 main()
