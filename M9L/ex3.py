d33ebb737f835273bb2d7d78883248d7dc88a6a0
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-20 15:45:35 -0300

    Update ex3.py

--- M9L/ex3.py
+++ M9L/ex3.py
@@ -1,16 +1,37 @@
 '''Escreva uma função que imprime, linha a linha, os valores de uma matriz
 bidimensional dada como argumento.
 '''
-def Matriz():
-    linhas = int(input("Insira a quantidade de linhas da matriz: "))
-    colunas = int(input("Insira a quantidade de colunas da matriz: "))
-   
-    matriz = []
-    for i in range(linhas):
-        linha = []
-        for j in range(colunas):
-            linha.append(int(input(f"Insira um valor numerico inteiro para {i,j}: ")))
-        matriz.append(linha)
-    print("Matriz = ", matriz)
+def criarMatriz():
+    while True:
+        try:
+            linhas = int(input("Insira a quantidade de linhas da matriz: "))
+            colunas = int(input("Insira a quantidade de colunas da matriz: "))
+            matriz = []
+            for i in range(linhas):
+                linha = []
+                for j in range(colunas):
+                    while True:
+                        try:
+                            linha.append(int(input(f"Insira um valor numerico inteiro para {i+1,j+1}: ")))
+                            break
+                        except ValueError:
+                            print("Valor Inválido")
+                matriz.append(linha)
+            return matriz
+        except ValueError:
+            print("Valores inválidos")
 
-Matriz()
+def mostrarMatrir(matriz):
+    print("-"*60)
+    for i in matriz:
+        print(i)
+    print("-"*60)
+
+
+def main():
+    matriz = criarMatriz()
+    mostrarMatrir(matriz)
+
+main()
+
+# Formatar o Output
