d79efb537b2b803497ccc8c0b543e09c23b78b5c
jptrava <164881489+jptrava@users.noreply.github.com>
2024-07-20 20:06:48 -0300

    Update ex6.py

--- M8L/ex6.py
+++ M8L/ex6.py
@@ -1,36 +1,38 @@
 '''6. Crie um programa que implemente uma função que, dada uma lista, retorne a moda
 da lista, ou seja, uma lista com o(s) elemento(s) mais frequente(s) da lista original.'''
-lista = []
 def moda(lista):
             
-            frequencia = {}
+    frequencia = {}
             
-            for elemento in lista:
-                if elemento in frequencia:
-                    frequencia[elemento] += 1
-                else:
-                    frequencia[elemento] = 1
+    for elemento in lista:
+        if elemento in frequencia:
+            frequencia[elemento] += 1
+        else:
+            frequencia[elemento] = 1
             
-            max_freq = max(frequencia.values())
+        max_freq = max(frequencia.values())
             
-            result = []
+        result = []
             
-            for elemento, freq in frequencia.items():
-                if freq == max_freq:
-                    result.append(elemento)
+        for elemento, freq in frequencia.items():
+            if freq == max_freq:
+                result.append(elemento)
             
-            return result
-print("Digite o numero e aperte ENTER,caso queira parar digite F:")
-while True:
-   
-    x = input()
-    if x.isnumeric():
-       
-        lista.append(int(x))
-    elif x == "F":
-        break
-    else:
-        print("Entrada invalida")
-        print("Digite novamente:")
-print(moda(lista))  
+        return result
+print(f"Digite o numero e aperte ENTER: \ncaso queira parar digite S:")
+
+def main():
+    lista=[]
+    while True:
+        x = input()
+        if x.isnumeric():
+        
+            lista.append(int(x))
+        elif x.upper() == "S":
+            print(moda(lista))
+            break
+        else:
+            print("Entrada invalida")
+            print("Digite novamente:")    
+main()
         
