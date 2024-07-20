29f888fd897d1ea91cad06cd9f708a47c9170365
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-20 16:11:10 -0300

    Update ex5.py

--- M9L/ex5.py
+++ M9L/ex5.py
@@ -1,25 +1,35 @@
 '''Escreva uma função que recebe duas matrizes (A e B). Se as duas matrizes tiverem
 dimensões compatíveis, sua função deve retornar a soma das duas (C = A + B). Caso
 contrário, sua função deve retornar uma lista vazia.'''
+
 def matriz(nome):
-    linhas = int(input(f"Informe o número de linhas da matriz {nome}: "))
-    colunas = int(input(f"Informe o número de colunas da matriz {nome}: "))
-    matriz = []
+    while True:
+        try:
+            linhas = int(input(f"Informe o número de linhas da matriz {nome}: "))
+            colunas = int(input(f"Informe o número de colunas da matriz {nome}: "))
+            matriz = []
 
-    print(f"Informe os elementos da matriz {nome}:")
-    for i in range(linhas):
-        linha = []
-        for j in range(colunas):
-            elemento = int(input(f"Insira o elemento ({i},{j}) da matriz {nome}: "))
-            linha.append(elemento)
-        matriz.append(linha)
-    return matriz
+            print(f"Informe os elementos da matriz {nome}:")
+            for i in range(linhas):
+                linha = []
+                for j in range(colunas):
+                        while True:
+                            try:
+                                elemento = int(input(f"Insira o elemento ({i+1},{j+1}) da matriz {nome}: "))
+                                linha.append(elemento)
+                                break
+                            except ValueError:
+                                print("Valor inválido")
+                matriz.append(linha)
+            return matriz
+        except ValueError:
+            print("Valor inválido")
 
-def SomaMatriz(A, B):
+def somaMatriz(A, B):
     if len(A) != len(B) or len(A[0]) != len(B[0]):  # verifica se as dimensões das matrizes são compatíveis
         return []
 
-    C = [[0 for _ in range(len(A[0]))] for _ in range(len(A))]  # matriz resultante C com zeros
+    C = [[0 for j in range(len(A[0]))] for i in range(len(A))]  # matriz resultante C com zeros
 
     for i in range(len(A)):  # soma das matrizes
         for j in range(len(A[0])):
@@ -32,7 +42,7 @@ def imprimir_matriz(matriz, nome):
     for linha in matriz:
         print(linha)
 
-def Main():
+def main():
 
     A = matriz("A")
     B = matriz("B")
@@ -42,11 +52,11 @@ def Main():
     imprimir_matriz(B, "B")
 
     # Soma das matrizes
-    resultado = SomaMatriz(A, B)
+    resultado = somaMatriz(A, B)
     if resultado:
         print("A soma das matrizes é:")
         imprimir_matriz(resultado, "C")
     else:
         print("As matrizes têm dimensões incompatíveis.")
 
-Main()
+main()
