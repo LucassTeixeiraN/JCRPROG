e7caf5c041844e4b680b128dbc33fabef4e8f90e
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-20 14:53:43 -0300

    Create 13b.py

---
+++ M8L/13b.py
@@ -0,0 +1,37 @@
+'''Uma equação do segundo grau é escrita
+ax**2 + bx + c = 0
+e a sua solução é dada em
+função dos valores de a, b e c. Podendo ter duas raízes, uma ou nenhuma. Escreva
+uma função que resolva a equação do segundo grau, retornando o número de raízes
+encontradas. Os valores dessas raízes devem ser retornados em parâmetros.'''
+
+
+def equacao(a, b, c):
+    
+    delta = b**2 - 4*a*c
+
+    if delta > 0:
+        raiz1 = (-b + delta**0.5) / (2*a)
+        raiz2 = (-b - delta**0.5) / (2*a)
+        print(f"A equação tem duas raízes reais:")
+        print(f"Raiz 1 (x1) = {raiz1}")
+        print(f"Raiz 2 (x2) = {raiz2}")
+    elif delta == 0:
+        raiz = -b / (2*a)
+        print(f"A equação tem uma raiz real (raiz dupla):")
+        print(f"Raiz (x) = {raiz}")
+    else:
+        print("A equação não tem raízes reais.")
+
+
+def main():
+    try:
+        a = float(input("Informe o valor de a: "))
+        b = float(input("Informe o valor de b: "))
+        c = float(input("Informe o valor de c: "))
+    except ValueError:
+        print("Valores inválidos")
+
+    equacao(a, b, c)
+
+main()
