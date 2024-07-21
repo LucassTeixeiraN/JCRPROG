04694119aed105cefc388ff44c7e760048796981
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-21 08:45:26 -0300

    Update ex1.py

--- M8L/ex1.py
+++ M8L/ex1.py
@@ -7,8 +7,9 @@ Divisor Comum (MDC) entre eles. A seguir, escreva um programa para testar essas
 funções.'''
 
 def mdc(a, b):
-
-    while b:
+    # Teorema de Euclides
+    while b != 0:
+        # As variáveis são salvas simultaneamente
         a, b = b, a % b
     return a
 
@@ -19,20 +20,55 @@ def mdc_lista(numeros):
         resultado = mdc(resultado, num)
     return resultado
 
-def main():
-    # Teste com dois numeros
-    a = input("Digite o primeiro numero: ")
-    b = input("Digite o segundo numero: ")
-    if a.isdigit() and b.isdigit():
-        a = int(a)
-        b = int(b)
-        
-        print(f"MDC({a}, {b}) = {mdc(a, b)}")
-    else:
-        print("Insira um numeros validos")
+def menu():
+    while True:
+        if input("Pressione ENTER para voltar ao Menu") == "":
+            print("-"*60)
+            print("Selecione uma opção:")
+            print("1 - MDC de dois números:")
+            print("2 - MDC de uma lista de números:")
+            print("0 - Sair do programa\n")
+            opcao = input()
 
-    # Teste com lista de numeros
-    numeros = [12, 18, 24, 30]
-    print(f"MDC da lista {numeros} = {mdc_lista(numeros)}")
-if __name__ == "__main__":
-    main()
+            # MDC de 2 números
+            if opcao == "1":
+                print("-"*60)
+                a = input("Digite o primeiro numero: ")
+                b = input("Digite o segundo numero: ")
+                if a.isdigit() and b.isdigit():
+                    a = int(a)
+                    b = int(b)
+            
+                    print(f"MDC({a}, {b}) = {mdc(a, b)}")
+                    print()
+                else:
+                    print("Insira um numeros validos")
+
+
+            #MDC de uma lista de números
+            elif opcao == "2":
+                print("Digite os números que irão compor sua lista (Pressione ESPAÇO entre cada um): ")
+                numeros = input()
+
+                while True:
+                    try:
+                        lista = [int(num) for num in numeros.split()]
+                        mdcs = mdc_lista(lista)
+                        print(f'MDC de {lista}: {mdcs}')
+                        print()
+                        break
+                    except ValueError:
+                        print("Valores inválidos")
+                        break
+
+            # Sair do programa
+            elif opcao == "0":
+                print("Saindo do programa")
+                print("-"*60)
+                break
+
+def main():
+
+    menu()
+
+main()
