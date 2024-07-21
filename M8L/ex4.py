c400fa831c029679a4460c98edebc7abaef72e75
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-21 15:37:09 -0300

    Update ex4.py

--- M8L/ex4.py
+++ M8L/ex4.py
@@ -5,23 +5,33 @@ combinação:
 ___________
 (𝑚 − 𝑛)! 𝑛!
 '''
+
+# Calcula o fatorial de um número. 
 def fatorial(num):
     if num == 0 or num == 1:
         return 1
     else:
-        return num * fatorial(num - 1)
+        fatorial = 1
+        for i in range(1, num+1):
+            fatorial *= i
+        return fatorial
 
+# Calcula o número de combinações possíveis de m elementos em grupos de n elementos usando a fórmula de combinação
 def combinacoes(m, n):
     return fatorial(m) // (fatorial(m - n) * fatorial(n))
 
 def main():
-    m = int(input("Digite o valor de m: "))
-    n = int(input("Digite o valor de n: "))
-    if n <= m:
-        print(f"O número de combinações possíveis de {m} elementos em grupos de {n} elementos é: {combinacoes(m, n)}")
-    else:
-        print("Erro: n deve ser menor ou igual a m.")
+    print("-"*60)
+    while True:
+        try:
+            m = int(input("Digite o valor de m: "))
+            n = int(input("Digite o valor de n: "))
+            if n <= m:
+                print(f"\nO número de combinações possíveis de {m} elementos em grupos de {n} elementos é: {combinacoes(m, n)}")
+            else:
+                print("\nn deve ser menor ou igual a m.")
+            print("-"*60)
+        except ValueError:
+            print("Valor inválido")
 
-if __name__ == "__main__":
-    main()
-
+main()
