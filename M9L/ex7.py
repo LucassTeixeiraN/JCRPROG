a94d5c6fa4e55e517a03fd57043fb30bc702530d
jptrava <164881489+jptrava@users.noreply.github.com>
2024-07-21 19:01:54 -0300

    Update ex7.py

--- M9L/ex7.py
+++ M9L/ex7.py
@@ -2,48 +2,48 @@
 diagonal.'''
 
 def lerMatriz():
-    lista = []
+    lista = []  # Cria uma lista vazia para armazenar as linhas da matriz
 
+    # Solicita ao usuário que insira a matriz, linha por linha
     print("Insira uma lista bidimensional colocando espaços entre os elementos de uma linha e apertando ENTER entre cada linha")
     print("Obs: Deixe a linha vazia quando não houver mais elementos a serem lidos")
     while True:
         try:
-            linha = input()
+            linha = input()  # Lê uma linha da entrada
             if linha == "":
-                break
-            valor = [int(numero) for numero in linha.split()]
+                break  # Se a linha estiver vazia, encerra a leitura
+            valor = [int(numero) for numero in linha.split()]  # Converte os números da linha para inteiros
 
-            lista.append(valor)
+            lista.append(valor)  # Adiciona a linha à lista
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido")  # Trata exceções caso o usuário insira um valor não inteiro
             
     return lista
 
 def mostrarMatriz(matriz):
-    print("-"*60)
+    print("-" * 60)
     for i in matriz:
-        print(i)
+        print(i)  # Imprime cada linha da matriz
     print()
 
 def matrizDiagonal(matriz):
-   
     tamanho = len(matriz)
     
     for i in range(tamanho):
         for j in range(tamanho):
             if i != j and matriz[i][j] != 0:
-                return False
-    return True
+                return False  # Se encontrar um elemento diferente de zero fora da diagonal, retorna False
+    return True  # Se não encontrar nenhum elemento diferente de zero fora da diagonal, retorna True
 
 def main():
-    matriz = lerMatriz()
+    matriz = lerMatriz()  # Lê a matriz do usuário
 
-    mostrarMatriz(matriz)
-    if matrizDiagonal(matriz) == True:
+    mostrarMatriz(matriz)  # Mostra a matriz na tela
+    if matrizDiagonal(matriz):
         print("A matriz é diagonal")
     else:
         print("A matriz não é diagonal")
-    print("-"*60)
+    print("-" * 60)
 
 main()
 
