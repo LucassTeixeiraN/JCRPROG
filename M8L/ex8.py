71baa6bd5cd82552cffcf631aa80e01a20fa2a29
Andrey Rodrigues <134998417+AndreyRodr@users.noreply.github.com>
2024-07-21 10:37:10 -0300

    Update ex8.py

--- M8L/ex8.py
+++ M8L/ex8.py
@@ -4,22 +4,26 @@ A seguir, faça um programa que, em
 loop, leia um valor para F da entrada padrão e o imprima o valor de C correspondente,
 utilizando a função FparaC.'''
 
+# Função para converter temperatura de Fahrenheit para Celsius
 def FparaC(F):
-    C = 5/9*(F-32)
+    C = 5/9*(F-32)  # Fórmula de conversão de Fahrenheit para Celsius
     return C
 
+# Função principal para ler a entrada do usuário e exibir a conversão
 def main():
     while True:
         try:
             print(70*"-")
             F = input("Digite os graus em °F (Deixe em branco para sair do programa): ")
-            if F == "":
+            if F == "":  # Verifica se o usuário deseja encerrar o programa
                 print("\nEncerrado!")
                 break
-            F = int(F)
-            print(f"\nConversao para graus °C: {FparaC(F):.2f}")
-        except ValueError:       
-            print("\nDados invalidos")
+            F = int(F)  # Converte a entrada para um número inteiro
+            print(f"\nConversao para graus °C: {FparaC(F):.2f}")  # Exibe a conversão para Celsius
+        except ValueError:
+            print("\nDados invalidos")  # Mensagem de erro para entradas não inteiras
             print(70*"-")
-                
+
+# Chama a função principal se o script for executado diretamente
 main()
+
