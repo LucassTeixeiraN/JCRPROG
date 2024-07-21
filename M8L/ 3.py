f0cdbfec288ab182883d081d40cb7e90a5bd0a77
Andrey Rodrigues <134998417+AndreyRodr@users.noreply.github.com>
2024-07-21 10:25:52 -0300

    Update  3.py

--- M8L/ 3.py
+++ M8L/ 3.py
@@ -1,30 +1,27 @@
-'''3. Escreva um programa com uma função que, dado um número inteiro (n > 1), retorne
+""'''3. Escreva um programa com uma função que, dado um número inteiro (n > 1), retorne
 uma lista com os fatores primos de n.
 '''
-#Dentro do loop principal, temos um loop interno que verifica se n é divisível pelo divisor atual (n % divisor == 0).
-#  Se for, adicionamos o divisor à lista fatores e dividimos n pelo divisor usando a divisão inteira (n //= divisor). 
-# Este processo é repetido até que n não seja mais divisível pelo divisor atual.
+# Função para calcular os fatores primos de um número
 def fatores_primos(n):
-    fatores = []
-    divisor = 2
-    while n > 1:
-        while n % divisor == 0:
-            fatores.append(divisor)
-            n //= divisor
-        divisor += 1
-    return fatores
+    fatores = []  # Lista para armazenar os fatores primos
+    divisor = 2  # Começa com o menor número primo
+    while n > 1:  # Continua até que n seja reduzido a 1
+        while n % divisor == 0:  # Verifica se o divisor atual é um fator de n
+            fatores.append(divisor)  # Adiciona o divisor à lista de fatores
+            n //= divisor  # Divide n pelo divisor
+        divisor += 1  # Incrementa o divisor para verificar o próximo número
+    return fatores  # Retorna a lista de fatores primos
 
+# Função principal para obter a entrada do usuário e exibir os fatores primos
 def main():
 
 # numero de entrada do usuario
     while True:
         try:
-            numero=input('insira um numero para descobrir os fatores primos dele (pressione ENTER para finalizar o programa): ')
-            if numero == "":
-                break
-            numero = int(numero)
-
-            print(fatores_primos(numero)) 
+            numero = int(input('insira um numero para descobrir os fatores primos dele: '))
+            print(fatores_primos(numero))  # Exibe os fatores primos do número
         except ValueError:
-            print("Valor inválido")
+            print("Valores invalidos!")  # Mensagem de erro para entradas não inteiras
+
+# Chama a função principal se o script for executado diretamente
 main()
