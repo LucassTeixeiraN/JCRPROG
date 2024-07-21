475d4c2cf95d734fa5e6831c8fe462b61c0f810a
jptrava <164881489+jptrava@users.noreply.github.com>
2024-07-21 18:52:02 -0300

    Update 4.py

--- M9L/4.py
+++ M9L/4.py
@@ -1,15 +1,18 @@
 '''4. Escreva uma função que dada uma matriz (M), calcule a sua transposta (Mt).'''
 
-def matriz(n,m):
+def matriz(n, m):
+    # Cria uma matriz vazia
     matriz = []
     for i in range(n):
         linha = []
         for j in range(m):
+            # Solicita entrada do usuário e adiciona ao elemento da linha atual
             linha.append(input())
         matriz.append(linha)
     return matriz
 
 def Transposta(matriz):
+    # Obtém o número de linhas e colunas
     linhas = len(matriz)
     colunas = len(matriz[0])
     Mt = []
@@ -17,11 +20,11 @@ def Transposta(matriz):
     for j in range(colunas):
         nova_linha = []
         for i in range(linhas):
+            # Troca as linhas pelas colunas para criar a matriz transposta
             nova_linha.append(matriz[i][j])
         Mt.append(nova_linha)
         
     return Mt
-    
 
 def main():
     n = int(input("Digite o número de linhas da matriz: "))
@@ -30,7 +33,7 @@ def main():
     print("Digite os elementos da matriz:")
     M = matriz(n, m)
     
-    print("-"*60)
+    print("-" * 60)
     print("\nMatriz Original:")
     for linha in M:
         print(linha)
@@ -39,6 +42,6 @@ def main():
     Mt = Transposta(M)
     for linha in Mt:
         print(linha)
-    print("-"*60)
+    print("-" * 60)
 
 main()
