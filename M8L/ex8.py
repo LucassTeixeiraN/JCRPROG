4ebf37ebea970cbf272a147997f3e21d32588ab9
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-21 09:53:24 -0300

    Update ex8.py

--- M8L/ex8.py
+++ M8L/ex8.py
@@ -1,24 +1,25 @@
 '''Escreva uma função (FparaC) que receba uma temperatura em graus F e retorne a
-temperatura em graus C, sendo: C =5/9*(F-32)
- . A seguir, faça um programa que, em
+temperatura em graus C, sendo: C =5/9*(F-32).
+A seguir, faça um programa que, em
 loop, leia um valor para F da entrada padrão e o imprima o valor de C correspondente,
 utilizando a função FparaC.'''
 
 def FparaC(F):
     C = 5/9*(F-32)
     return C
+
 def main():
     while True:
         try:
-            F = int(input("Digite os graus em F:"))
-            print(50*"-")
+            print(70*"-")
+            F = input("Digite os graus em °F (Deixe em branco para sair do programa): ")
+            if F == "":
+                print("\nEncerrado!")
+                break
+            F = int(F)
+            print(f"\nConversao para graus °C: {FparaC(F):.2f}")
         except ValueError:       
-            print("Dados invalidos")
+            print("\nDados invalidos")
+            print(70*"-")
                 
-                    
-        print(f"Conversao para graus C:{FparaC(F):.2f}")
-        print(50*"-")
-           
-        print("Encerrado")
-        break
 main()
