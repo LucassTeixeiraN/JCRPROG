8303ca75070ccd5c1e62672d8e146f9ff9224e0b
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-07-21 10:13:43 -0300

    Update ex13.py

--- M8L/ex13.py
+++ M8L/ex13.py
@@ -5,7 +5,6 @@ função dos valores de a, b e c. Podendo ter duas raízes, uma ou nenhuma. Escr
 uma função que resolva a equação do segundo grau, retornando o número de raízes
 encontradas. Os valores dessas raízes devem ser retornados em parâmetros.'''
 
-
 def equacao(a, b, c):
     
     delta = b**2 - 4*a*c
@@ -13,26 +12,21 @@ def equacao(a, b, c):
     if delta > 0:
         raiz1 = (-b + delta**0.5) / (2*a)
         raiz2 = (-b - delta**0.5) / (2*a)
-        return 2, raiz1, raiz2
+        print(f"A equação tem duas raízes reais:")
+        print(f"Raiz 1 (x1) = {raiz1}")
+        print(f"Raiz 2 (x2) = {raiz2}")
     elif delta == 0:
         raiz = -b / (2*a)
-        return 1, raiz
+        print(f"A equação tem uma raiz real (raiz dupla):")
+        print(f"Raiz (x) = {raiz}")
     else:
-        return 0  # Nenhuma raiz real
+        print("A equação não tem raízes reais.")
 
-a = int(input("Informe o valor de a: "))
-b = int(input("Informe o valor de b: "))
-c = int(input("Informe o valor de c: "))
-resultado = equacao(a, b, c)
 
-if resultado[0] == 2:
-    raiz1, raiz2 = resultado[1], resultado[2]
-    print(f"A equação tem duas raízes reais:")
-    print(f"Raiz 1 (x1) = {raiz1}")
-    print(f"Raiz 2 (x2) = {raiz2}")
-elif resultado[0] == 1:
-    raiz = resultado[1]
-    print(f"A equação tem uma raiz real (raiz dupla):")
-    print(f"Raiz (x) = {raiz}")
-else:
-    print("A equação não tem raízes reais.")
+def main(): 
+    a = int(input("Informe o valor de a: "))
+    b = int(input("Informe o valor de b: "))
+    c = int(input("Informe o valor de c: "))
+    equacao(a, b, c)
+
+main()
