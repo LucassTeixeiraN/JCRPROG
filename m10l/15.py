62f9edd1b3d8c9d28a090224adb848c79c9ad4ab
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-08-01 19:45:53 -0300

    Update 15.py

--- m10l/15.py
+++ m10l/15.py
@@ -4,30 +4,29 @@
 # lista [[2,4,-3,-1],[-3,0,7],[-8,6]] a função deve devolver True pois todas as listas têm
 # igual número de elementos positivos e negativos.
 
+def conta_pos_neg(lista, pos = 0, neg = 0):
 
-def conta_pos_neg(lista):
-   
-    pos = 0 
-    neg = 0
-    for num in lista:
-        if num > 0:
-            pos += 1
-        elif num < 0:
-            neg += 1
-    return pos, neg
+    if len(lista) < 1:
+        return pos, neg
+    elif lista[0] > 0:
+        return conta_pos_neg(lista[1:], pos + 1, neg)
+    elif lista[0] < 0:
+        return conta_pos_neg(lista[1:], pos, neg + 1)
+    else:
+        return conta_pos_neg(lista[1:], pos, neg)
 
-def verifica_pos_neg_igual(lista_de_listas):
-   
-    if not lista_de_listas:
+def verifica_pos_neg_igual(lista):
+
+    if not lista:
         return True
+        
+    pos, neg = conta_pos_neg(lista[0])
+    
+    if pos == neg:
+        return verifica_pos_neg_igual(lista[1:])
+    else:
+        return False
 
-    for lista in lista_de_listas[1:]:
-        pos_lista, neg_lista = conta_pos_neg(lista)
-        if pos_lista != neg_lista:
-            return False
-
-   
-    return verifica_pos_neg_igual(lista_de_listas[1:])
 
 def main():
     entrada_usuario = input("Digite uma lista de listas de números inteiros (separe os elementos por vírgula e use ponto e virgula para separar as sublistas): ")
