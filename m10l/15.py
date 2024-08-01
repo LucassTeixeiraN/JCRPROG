da980004bfe4feb09a037752036d189c30f3d5aa
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-08-01 08:05:36 -0300

    Update 15.py

--- m10l/15.py
+++ m10l/15.py
@@ -7,7 +7,7 @@
 
 def conta_pos_neg(lista):
    
-    pos = 0
+    pos = 0 
     neg = 0
     for num in lista:
         if num > 0:
@@ -19,25 +19,23 @@ def conta_pos_neg(lista):
 def verifica_pos_neg_igual(lista_de_listas):
    
     if not lista_de_listas:
-     
         return True
-
-    pos_primeira_lista, neg_primeira_lista = conta_pos_neg(lista_de_listas[0])
 
     for lista in lista_de_listas[1:]:
         pos_lista, neg_lista = conta_pos_neg(lista)
-        if pos_lista != pos_primeira_lista or neg_lista != neg_primeira_lista:
+        if pos_lista != neg_lista:
             return False
 
    
     return verifica_pos_neg_igual(lista_de_listas[1:])
 
-
-entrada_usuario = input("Digite uma lista de listas de números inteiros (separe os elementos por vírgula e use ponto e virgula para separar as sublistas): ")
-try:
-   
-    lista_de_listas = [list(map(int, sublist.split(','))) for sublist in entrada_usuario.split(';')]
-    resultado = verifica_pos_neg_igual(lista_de_listas)
-    print(f"Resultado: {resultado}")
-except ValueError:
-    print("Entrada inválida. Certifique-se de inserir números inteiros separados por vírgula e listas separadas por ponto e vírgula.")
+def main():
+    entrada_usuario = input("Digite uma lista de listas de números inteiros (separe os elementos por vírgula e use ponto e virgula para separar as sublistas): ")
+    try:
+    
+        lista_de_listas = [list(map(int, sublist.split(','))) for sublist in entrada_usuario.split(';')]
+        resultado = verifica_pos_neg_igual(lista_de_listas)
+        print(f"Resultado: {resultado}")
+    except ValueError:
+        print("Entrada inválida. Certifique-se de inserir números inteiros separados por vírgula e listas separadas por ponto e vírgula.")
+main()
