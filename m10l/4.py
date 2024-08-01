ed7672f0a58c225da23421a9b942b55404927ee4
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2024-08-01 12:56:13 -0300

    Update 4.py

--- m10l/4.py
+++ m10l/4.py
@@ -6,23 +6,23 @@
 # contrário).
 
 
-def maximoLista(max, indice, lista):
+def maximoLista(lista, max = 0, indice = 0):
     if indice < len(lista):
         if max == 0 or lista[indice] > max:
             max = lista[indice]
             
-        return maximoLista(max, indice+1, lista)
+        return maximoLista(lista, max, indice+1)
     return max
 
-def listaNNumeros(max, n, lista):
+def listaNNumeros(n, lista, max = 0):
     if n >= 0:
         if max == 0 or lista[n] > max:
             max = lista[n]
 
-        return listaNNumeros(max, n-1, lista)
+        return listaNNumeros(n-1, lista, max)
     return max
 
-def buscarCaracter(string, carac, indice):
+def buscarCaracter(string, carac, indice = 0):
     if indice < len(string):
         if string[indice] == carac:
             return indice
@@ -43,16 +43,13 @@ def menu():
 def main():
 
     while True:
-        maximo = 0
-        indice = 0
-
         opcoes = menu()
 
         try:
             if opcoes == "1":
                 print("Insira números para uma lista (utilize espaço entre cada número e pressione ENTER quando não houver mais elementos): ")
                 lista = [int(num) for num in input().split()]
-                print(f"O máximo da lista inserida é: {maximoLista(maximo, indice, lista)}")
+                print(f"O máximo da lista inserida é: {maximoLista(lista)}")
 
             elif opcoes == "2":
                 n = int(input("Insira o número de elementos de sua lista: "))
@@ -60,12 +57,12 @@ def main():
                 print("Insira os elementos de sua lista apertando ENTER entre cada um: ")
                 for i in range(n):
                     lista.append(int(input()))
-                print(f"O máximo da lista inserida é: {listaNNumeros(maximo, n-1, lista)}")
+                print(f"O máximo da lista inserida é: {listaNNumeros(n-1, lista)}")
 
             elif opcoes == "3":
                 s = input("Insira uma palavra: ")
                 c = input("Insira um caractere para ser buscado na palavra anterior: ")
-                print(f"O caractere {c} está no index: {buscarCaracter(s, c, indice)}")
+                print(f"O caractere {c} está no index: {buscarCaracter(s, c)}")
 
             elif opcoes == "0":
                 print("Finalizando programa")
